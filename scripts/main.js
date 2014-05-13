c006d31565669c01a67ea0e8fb5ec9f2db70f97a
Loc_rabbirt <nguyenvantienloc@gmail.com>
2014-05-14 00:45:21 +0700

    Release day
    You can use it for your project

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+/*Progress Bar*/
+jQuery(document).ready(function($) {
+    var bar = $('.progress-bar');
+    $(function(){
+      $(bar).each(function(){
+        bar_width = $(this).attr('aria-valuenow');
+        $(this).width(bar_width + '%');
+      });
+    });
+});
